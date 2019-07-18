3d6711caac4158dd4d7e242657b24f47ce41f229
Harry Chopra <harrychopra@gmail.com>
2019-07-18 18:26:50 +0100

    Refactored code

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,20 @@
 class Dog
   # code goes here
+  attr_reader :name
+  attr_writer
+  attr_accessor  :mood, :owner
+  @@all = []
+  
+    def initialize(name, owner)
+      @owner = owner
+      @name = name
+      @mood = "nervous"
+      @@all << self
+      @owner.dogs << self
+    end
+  
+    def self.all
+      @@all
+    end
+    
 end
